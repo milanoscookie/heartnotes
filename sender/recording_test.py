5dcbf9f78fdceadf298cdaacc4d4f33997450e96
sofiaperelonia <sofiaperelonia2024@u.northwestern.edu>
2024-05-22 18:30:02 -0500

    initial

--- sender/recording_test.py
+++ sender/recording_test.py
@@ -5,7 +5,7 @@ import time
 import soundfile as sf
 
 # Set the duration of the recording in seconds
-DURATION = 5
+DURATION = 6
 
 # Define the sample rate and other audio parameters
 sample_rate = 44100
@@ -19,15 +19,17 @@ def callback(indata, frames, time, status):
     recorded_data.append(indata.copy())
 
 # Initialize an empty list to store recorded audio data
-recorded_data = []
 
-print('Recording started...')
-audio_data = sd.rec(int(DURATION * sample_rate), samplerate=sample_rate, channels=channels, device='hw:3,0')
-sd.wait()  # Wait until recording is finished
-print('Recording stopped')
+def record_stream():
+    recorded_data = []
 
-# Save the recorded audio to a WAV file
-output_file = 'recorded_audio.wav'
-sf.write(output_file, audio_data, sample_rate)
+    print('Recording started...')
+    audio_data = sd.rec(int(DURATION * sample_rate), samplerate=sample_rate, channels=channels, device='hw:3,0')
+    sd.wait()  # Wait until recording is finished
+    print('Recording stopped')
 
-print(f'Recording saved as {output_file}')
+    # Save the recorded audio to a WAV file
+    output_file = 'recorded_audio.wav'
+    sf.write(output_file, audio_data, sample_rate)
+
+    print(f'Recording saved as {output_file}')
